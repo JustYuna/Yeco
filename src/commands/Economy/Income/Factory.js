8ca9893658d5eb4b455ab4f7b4516c628b3070bf
JustYuna <sebastgian.xyz.huber@gmail.com>
2026-05-20 00:39:55 +0200

    feat: added factory.js file

---
+++ src/commands/Economy/Income/Factory.js
@@ -0,0 +1,21 @@
+// Factory.js
+
+const { GetAsync, SetAsync, AddToAsync } = require("../../../DataStorage/Datastore");
+
+async function Factory(interaction, client, { type }) {
+    switch(type) {
+        case "claim_income": {
+
+        }
+
+        case "upgrade": {
+
+        };
+
+        default: {
+
+        };
+    }
+}
+
+module.exports = Factory;
