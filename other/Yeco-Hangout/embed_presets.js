cf5aa2fc37c8d613e74e6596b8443357598e3700
Yuna <sebastian.xyz.huber@gmail.com>
2026-06-09 21:57:52 +0200

    chore(embed_presets.js): adjusted rule preset for server changes

--- other/Yeco-Hangout/embed_presets.js
+++ other/Yeco-Hangout/embed_presets.js
@@ -2,36 +2,38 @@ const { EmbedBuilder } = require("discord.js");
 
 const Presets = {
     rules: new EmbedBuilder()
-            .setColor('Aqua')
+            .setColor([150, 100, 250])
             .setTitle('📜 Server Rules')
-            .setAuthor({
-                name: 'Yuna',
-            })
-            .addFields(
-                {
-                    name: 'General Rules',
-                    value: `
-1. Be respectful — no harassment or personal attacks.
+            .setDescription(`
+📌 ~ General Rules ~
+
+1. Be respectful.
 2. No NSFW or clearly offensive content.
-3. Avoid spam, trolling, and disruptive behavior.
-4. Don't impersonate others or misuse alt accounts.
-5. Follow Discord's Terms of Service.
-6. Use English in public channels, including voice channels.
-7. Do not send AI-generated content.
-8. Promotion of any kind is forbidden.
-9. Sending scam or malicious content will result in a ban.
-                `,
-                },
-                {
-                    name: 'Regarding Yeco',
-                    value: `
-Terms of Service:
+3. Don't impersonate others or misuse alt accounts.
+4. Use English when chatting.
+5. Do not hold full-on conversations in the economy chat.
+6. Promotion of any kind is strictly forbidden.
+7. Sending scam or malicious content will result in an immediate ban.
+8. Use the designated channels for support requests. DMs to staff will be ignored.
+
+🔗 ~ Other things to follow ~
+
+Discord - Terms of Service:
+https://discord.com/terms
+
+⚙️ ~ For server owners using Yeco ~
+
+Yeco - Terms of Service:
 https://github.com/JustYuna/Yeco/blob/main/ToS.md
 
-Privacy Policy:
-https://github.com/JustYuna/Yeco/blob/main/PrivacyPolicy.md
-                `,
-                }),
+ℹ️ ~ Other information ~
+
+Having problems using Yeco?
+→ Use /report to submit an issue
+→ Or use /onboarding for a quick tutorial
+
+Yeco - Privacy Policy:
+https://github.com/JustYuna/Yeco/blob/main/PrivacyPolicy.md`)
 };
 
 module.exports = Presets;
