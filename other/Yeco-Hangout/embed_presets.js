b61380aceddb712e1251023417bb70164b250a5e
Yuna <sebastian.xyz.huber@gmail.com>
2026-06-19 17:00:28 +0200

    chore(embed_presets): adjusted rules to new server

--- other/Yeco-Hangout/embed_presets.js
+++ other/Yeco-Hangout/embed_presets.js
@@ -5,26 +5,16 @@ const Presets = {
             .setColor([150, 100, 250])
             .setTitle('📜 Server Rules')
             .setDescription(`
-📌 ~ General Rules ~
-
-1. Be respectful.
-2. No NSFW or clearly offensive content.
-3. Don't impersonate others or misuse alt accounts.
-4. Use English when chatting.
-5. Do not hold full-on conversations in the economy chat.
-6. Promotion of any kind is strictly forbidden.
-7. Sending scam or malicious content will result in an immediate ban.
-8. Use the designated channels for support requests. DMs to staff will be ignored.
-
-🔗 ~ Other things to follow ~
-
-Discord - Terms of Service:
-https://discord.com/terms
 
 ⚙️ ~ For server owners using Yeco ~
 
 Yeco - Terms of Service:
 https://github.com/JustYuna/Yeco/blob/main/ToS.md
+
+🔗 ~ Other things to follow ~
+
+Discord - Terms of Service:
+https://discord.com/terms
 
 ℹ️ ~ Other information ~
 
