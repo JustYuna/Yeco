93b4d4ea0095bd75da1fa40bfbeedf30f03c5dc8
Yuna <sebastian.xyz.huber@gmail.com>
2026-06-19 18:36:18 +0200

    feat(Gift.js): added the missing check for 'TOO_RICH'

--- src/commands/Economy/General/Gift.js
+++ src/commands/Economy/General/Gift.js
@@ -11,7 +11,8 @@ async function gift(interaction, client, user, amount) {
     const userId = interaction.user.id;
     const giftUserId = user.id;
 
-    const userBalance = await GetAsync(interaction.user.id, "MAIN_CURRENCY")
+    const userBalance = await GetAsync(userId, "MAIN_CURRENCY");
+    const giftedUserBalance = await GetAsync(giftUserId, "MAIN_CURRENCY");
 
     // Sanity check: prevent gifting to bots
     if (user.bot) {
@@ -31,6 +32,9 @@ async function gift(interaction, client, user, amount) {
     // Finalize validation
     const validateError = await CommandHelper.VALIDATE_CURRENCY(interaction, amount, { min: MIN, max: MAX, userBalance: userBalance, command: "gift" });
     if (validateError) return;
+
+    if (giftedUserBalance > GIFT.MAX_RECIEVER)
+        return interaction.editReply(ConfigManager.getMsg("ECONOMY.GIFT.MESSAGES.TOO_RICH"));
 
     // Execute gift
     await AddToAsync(userId, { MAIN_CURRENCY: -amount });
