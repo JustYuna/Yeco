f82cee2fff9d070193e972fd18b4d2d6340dc155
Yuna <sebastian.xyz.huber@gmail.com>
2026-06-26 20:09:56 +0200

    feat(VisualizeBar): percentages are rounded now + added desription

--- src/Utilities/Visual/VisualizeBar.js
+++ src/Utilities/Visual/VisualizeBar.js
@@ -1,17 +1,26 @@
+/**
+ * Generates a bar to visualize a percentage
+ * other maid commands using the assigned snowflake or included entryName.
+ * @param {number} value
+ * @param {string} vallabelue
+ * @param {number} barCount
+ * @param {boolean} valushowPercentagee
+ */
 function VisualizeBar({ value, label, barCount, showPercentage }) {
     const totalBars = barCount || 20;
     const filled = Math.min(totalBars, Math.round((value / 100) * totalBars));
     const bar = "▮".repeat(filled) + "▯".repeat(totalBars - filled);
+    const percentage = Math.round(value * 100) / 100;
 
     if (label) {
         if (showPercentage) {
-           return `${label}: [${bar}] (${value}%)`; 
+            return `${label}: [${bar}] (${percentage}%)`;
         } else {
             return `${label}: [${bar}]`;
         }
     } else {
         if (showPercentage) {
-           return `[${bar}] (${value}%)`; 
+            return `[${bar}] (${percentage}%)`;
         } else {
             return `[${bar}]`;
         }
